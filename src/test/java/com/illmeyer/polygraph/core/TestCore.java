755cc9ed35b7606b7ac32ce2f5daedf8b1eb2f50
Wolfgang Illmeyer <wolfgang@illmeyer.com>
2011-10-30 14:34:07 +0100

    actual tests

---
+++ src/test/java/com/illmeyer/polygraph/core/TestCore.java
@@ -0,0 +1,55 @@
+package com.illmeyer.polygraph.core;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import freemarker.cache.TemplateLoader;
+import static org.mockito.Mockito.stub;
+
+import static org.mockito.Mockito.verify;
+
+import static org.mockito.Mockito.mock;
+public class TestCore {
+	@Test
+	public void executeGunTest() throws IOException {
+		
+		Gun g = new Gun(getMockedAddressSupplier(),getMockedMessageDispatcher(),"/tpl/test/main",null,null,getMockedTemplateLoader());
+
+		g.trigger();
+	}
+	
+	public AddressSupplier getMockedAddressSupplier() {
+		Address a = new Address();
+		a.getAddrs().put("email", "wolfgang@illmeyer.com");
+		a.getFields().put("firstname","Wolfgang");
+		
+		AddressSupplier asup = mock(AddressSupplier.class);
+		stub(asup.hasMoreElements()).toReturn(true).toReturn(false);
+		stub(asup.nextElement()).toReturn(a);
+		return asup;
+	}
+	
+	public TemplateLoader getMockedTemplateLoader() throws IOException {
+		TemplateLoader tl = mock(TemplateLoader.class);
+		stub(tl.findTemplateSource("tpl/test/main")).toReturn("main");
+		stub(tl.getReader("main", "UTF-8")).toReturn(new StringReader("Hallo ${addr.fields.firstname}!"));
+		return tl;
+	}
+	
+	public MessageDispatcher getMockedMessageDispatcher() {
+		MessageDispatcher md= new MessageDispatcher() {
+			
+			public void initialize() {
+			}
+			
+			public void dispatchMessage(String message) {
+				System.out.println("Received Message: "+message);
+				Assert.assertEquals("Hallo Wolfgang!",message);
+			}
+		};
+		return md;
+	}
+}
---
+++ src/test/java/com/illmeyer/polygraph/core/TestTemplateLoader.java
@@ -0,0 +1,24 @@
+package com.illmeyer.polygraph.core;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TestTemplateLoader {
+	@Test
+	public void testURLs() throws IOException {
+		File f = new File("/tmp");
+		MessageGunTemplateLoader tl = new MessageGunTemplateLoader(f);
+		Object u = tl.findTemplateSource("sys/folder/file");
+		Assert.assertEquals("file:/tmp/sys.jar!/vfs/folder/file", u.toString());
+		u= tl.findTemplateSource("ext/archive/folder/file");
+		Assert.assertEquals("file:/tmp/ext/archive.jar!/vfs/folder/file", u.toString());
+		u= tl.findTemplateSource("tpl/archive/folder/file");
+		Assert.assertEquals("file:/tmp/tpl/archive.jar!/vfs/folder/file", u.toString());
+		u= tl.findTemplateSource("mt/archive/folder/file");
+		Assert.assertEquals("file:/tmp/mt/archive.jar!/vfs/folder/file", u.toString());
+	}
+}
