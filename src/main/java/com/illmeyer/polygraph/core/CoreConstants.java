fdcd19d626803f205e047d0978cffe13624e9221
Wolfgang Illmeyer <wolfgang@illmeyer.com>
2013-07-02 23:11:50 +0200

    remove deprecated constant

--- src/main/java/com/illmeyer/polygraph/core/CoreConstants.java
+++ src/main/java/com/illmeyer/polygraph/core/CoreConstants.java
@@ -26,11 +26,6 @@ public final class CoreConstants {
 	public static final String ECA_PARTS="polygraph.syslib.parts";
 	
 	/**
-	 * Used as key to a custom attribute in the freemarker Environment to store the currently active MessagePart
-	 */
-	public static final String ECA_CURRENT_PART="polygraph.syslib.currentPart";
-	
-	/**
 	 * Stack of Tags
 	 */
 	public static final String ECA_TAGSTACK="polygraph.core.tagStack";
